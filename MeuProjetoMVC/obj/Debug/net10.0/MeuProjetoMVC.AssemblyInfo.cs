ec61b5917fca1d8f1cbd228e40cd1a0d35029202
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-06-03 15:34:57 +0000

    ...

--- MeuProjetoMVC/obj/Debug/net10.0/MeuProjetoMVC.AssemblyInfo.cs
+++ MeuProjetoMVC/obj/Debug/net10.0/MeuProjetoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5d6e1877584748a2d19b8f1e2577c6972acc5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80125eae3c3e23a33e5f16a949e35ed92687be8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
