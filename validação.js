c6451c08c150928f4bf54601ab905921de30d55f
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-06-03 01:40:40 +0000

    ...

---
+++ validação.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById("meuformulario");
+const ElementoErro = document.getElementById("erro");
+
+formulario.addEventListener("submit", function(event) {
+
+const nome = document.getElementById("nome").value.trim(); 
+const email = document.getElementById("email").value.trim();
+const idade = document.getElementById("idade").value;
+
+ElementoErro.textContent = "";
+let erros = [];
+
+if (nome === "") {
+    erros.push("O campo (Nome) não pode estar vazio.");
+}
+
+if (!email.includes("@")) {
+    erros.push("O (Email) deve conter o caractere '@'.");
+}
+
+if ( idade === "" || Number(idade) <= 0) {
+    erros.push("A (idade) precisa ser maior que 0 !!!");
+}
+
+if (erros.length > 0) {
+    event.preventDefault();
+    ElementoErro.textContent = erros.join(" | ");
+} else {
+    alert("Formulário validado com sucesso");
+    }
+});
