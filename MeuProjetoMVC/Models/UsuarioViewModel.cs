6a1696a542c651ef99092728e1179e9f3ccf536e
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-06-03 02:32:06 +0000

    ....

---
+++ MeuProjetoMVC/Models/UsuarioViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuProjetoMVC.Models
+{
+    public class UsuarioViewModel
+{
+    public string Nome {get; set;}
+    public string Email {get; set;} 
+    public int Idade {get; set;}    
+    }
+}
